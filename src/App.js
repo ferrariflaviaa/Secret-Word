e76598f9401d11de067109a4425635b2e07e3a7b
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-04-17 21:36:16 -0300

    feat: project components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 import { StartScreen } from './Components/StartScreen'
+import { wordsList } from './Data/words'
+import { Game } from './Components/Game'
+import { GameOver } from './Components/GameOver'
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words, setWords] = useState(wordsList)
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
