9c8db191025b587a189236ddc3518a5a66fdb856
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-04-17 22:13:27 -0300

    feat: function that generates category and random word

--- src/App.js
+++ src/App.js
@@ -13,28 +13,54 @@ const stages = [
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
-  const [words, setWords] = useState(wordsList);
+  const [words, setWords] = useState(wordsList)
+  const [pickedWordm, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
 
+  const pickWordCategory = () => {
+    //pink a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //pink a random word
+    const word =
+      words[category][Math.floor(Math.random() * Object.keys(category).length)]
+
+    return {word, category}
+  }
   //starts the secret word game
   const startGame = () => {
-    setGameStage(stages[1].name);
+    //pick word pick category
+    const { word, category } = pickWordCategory()
+
+    //create an array of letter
+    // let wordLetters = word.split("")
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+    setGameStage(stages[1].name)
   }
 
   //process the letter input
   const verifyLetter = () => {
-    setGameStage(stages[2].name);
+    setGameStage(stages[2].name)
   }
 
   //restarts the game
   const retry = () => {
-    setGameStage(stages[0].name);
+    setGameStage(stages[0].name)
   }
 
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
