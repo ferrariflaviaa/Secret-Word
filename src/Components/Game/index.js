68c2ea0b0f66b647e9ba093b5f1b620066b09e73
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-04-20 14:39:25 -0300

    feat: game component structure

--- src/Components/Game/index.js
+++ src/Components/Game/index.js
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3>
+        Diga sobre a palavra: <span>Diga...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="blankSquare">
+        <p>Tente adivinhar uma letra da palavra</p>
+        <form>
+        <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+          />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className='wrongLettersContainer'>
+      <p>Letras já utilizadas:</p>
+      <span>a</span>
+      </div>
     </div>
   )
 }
